1d020035cfb358c39f14d5e6401d654a310e7d5d
ThuKhaCharmThar <thukhachanthar.dev@example.com>
2024-02-26 15:37:18 +0630

    edit ui

--- src/App.js
+++ src/App.js
@@ -1,69 +1,88 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import {
-  CheckIcon,
-  MinusIcon,
-  PlusIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/solid";
+import { MinusIcon, PlusIcon, TrashIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import Button from "react-bootstrap/Button";
 
 function App() {
   let [num1, setnum1] = useState();
   let [num2, setnum2] = useState();
-  let [sum, setSum] = useState();
-  let [sub, setSub] = useState();
-  let [div, setDiv] = useState();
-  let [mul, setMul] = useState();
-  let [result, setResult] = useState();
+  let [result, setResult] = useState(null);
   let [operation, setOperation] = useState(null);
 
-  // Sum Function
+  // ! Delete Function
+  let clearAll = () => {
+    setnum1('');
+    setnum2('');
+    setResult(null);
+    setOperation(null);
+  };
+
+  // * Sum Function
   let sumNum = () => {
-    setResult(parseInt(num1) + parseInt(num2));
+    if (!num1 || !num2) {
+      setResult("Please enter both numbers");
+      return;
+    }
+    setResult(parseFloat(num1) + parseFloat(num2));
     setOperation("Sum");
   };
 
-  // Sub Function
+  //* Subtract Function
   let subNum = () => {
-    if (parseInt(num1) < parseInt(num2)) {
+    if (!num1 || !num2) {
+      setResult("Please enter both numbers");
+      return;
+    }
+    if (parseFloat(num1) < parseFloat(num2)) {
       // Display message when num2 is less than num1
       setResult("Cannot subtract, first number is smaller");
       setOperation("Subtract");
     } else {
-      setResult(parseInt(num1) - parseInt(num2));
+      setResult(parseFloat(num1) - parseFloat(num2));
       setOperation("Subtract");
     }
   };
 
-  // Div Function
+  // * Division Function
   let divNum = () => {
-    if (parseInt(num2) === 0) {
+    if (!num1 || !num2) {
+      setResult("Please enter both numbers");
+      return;
+    }
+    if (parseFloat(num2) === 0) {
       setResult("Cannot divide by zero Idiot!");
       setOperation("Division");
     } else {
-      setResult(parseInt(num1) / parseInt(num2));
+      setResult(parseFloat(num1) / parseFloat(num2));
       setOperation("Division");
     }
   };
-  // Mul Function
+
+  // * Multiplication Function
   let mulNum = () => {
-    setResult(parseInt(num1) * parseInt(num2));
+    if (!num1 || !num2) {
+      setResult("Please enter both numbers");
+      return;
+    }
+    setResult(parseFloat(num1) * parseFloat(num2));
     setOperation("Multiplication");
   };
 
   useEffect(() => {
-    console.log("rander again");
+    console.log("Hello Rander");
   }, []);
 
   return (
     <div className="App">
       <div className="con">
         <h1>Calculator</h1>
-        <div>
-          <span className="fs-5">First Number</span>
+        <div className=" mt-5">
+          {/* <span className="fs-5">First Number</span> */}
           <input
             className="input"
+            inputMode="decimal"
+            value={num1}
+            required
             type="number"
             placeholder="Enter First Number"
             onChange={(change) => {
@@ -71,32 +90,58 @@ function App() {
             }}
           ></input>
         </div>
-
-        <div>
-          <span className="fs-5">Second Number</span>
+        <div className=" mt-3">
+          {/* <span className="fs-5">Second Number</span> */}
           <input
             type="number"
+            value={num2}
+            inputMode="decimal"
             placeholder="Enter Second Number"
+            required
             onChange={(change) => {
               setnum2(change.target.value);
             }}
           ></input>
         </div>
-
         <div className="mt-3">
-          <Button className="btn me-lg-4" style={{ backgroundColor:'#8cb0b6' }} onClick={sumNum}>
+          <Button
+            className="btn me-lg-3 rounded-circle"
+            style={{ backgroundColor: "#8cb0b6" }}
+            onClick={sumNum}
+          >
             <PlusIcon width={20} />
           </Button>
-          <Button className="btn me-lg-4"  style={{ backgroundColor:'#8cb0b6' }} onClick={subNum}>
+          <Button
+            className="btn me-lg-3 rounded-circle"
+            style={{ backgroundColor: "#8cb0b6" }}
+            onClick={subNum}
+          >
             <MinusIcon width={20} />
           </Button>
-          <Button className="btn me-lg-4" style={{ backgroundColor:'#8cb0b6' }}  onClick={mulNum}>
+          <Button
+            className="btn me-lg-3 rounded-circle"
+            style={{ backgroundColor: "#8cb0b6" }}
+            onClick={mulNum}
+          >
             <XMarkIcon width={20} />
           </Button>
-          <Button className="btn me-lg-4" style={{ backgroundColor:'#8cb0b6', height:'38px', width:'46px'}} onClick={divNum}>
-            {/* <span style={{ fontSize:'23px'}}> */}
+          <Button
+            className="btn me-lg-3 rounded-circle"
+            style={{
+              backgroundColor: "#8cb0b6",
+              height: "38px",
+              width: "46px",
+            }}
+            onClick={divNum}
+          >
             ÷
-            {/* </span> */}
+          </Button>
+          
+          <Button
+            className="btn btn-dark rounded-circle me-lg-3"
+            onClick={clearAll}
+          >
+            <TrashIcon width={20} />
           </Button>
           {operation && (
             <h5 className="mt-2">
